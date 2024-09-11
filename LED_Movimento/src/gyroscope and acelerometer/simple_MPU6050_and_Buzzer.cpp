26b69bd23f4d220d72e015460367a2f417e9e621
mthxz <matheus.sinto@unesp.br>
2024-09-11 15:49:15 -0300

    Add gyroscope and accelerometer code for LED movement

--- LED_Movimento/src/gyroscope and acelerometer/simple_MPU6050_and_Buzzer.cpp
+++ LED_Movimento/src/gyroscope and acelerometer/simple_MPU6050_and_Buzzer.cpp
@@ -14,7 +14,16 @@
 #include "pitches.h"
 
 // ESP32 pin GPIO18 connected to piezo buzzer
-#define BUZZZER_PIN_1  18 
+#define BUZZZER_PIN_1  25
+#define BUZZZER_PIN_2  26
+
+struct note
+{
+  int pitch;
+  int octave;
+  int duration;
+};
+
 
 // defines  Bb Major scale with 6 octaves
 int bb_scale[6][8] = {{NOTE_AS1, NOTE_C1, NOTE_D1, NOTE_DS1, NOTE_F1, NOTE_G1, NOTE_A1, SILENCE}, 
@@ -24,51 +33,13 @@ int bb_scale[6][8] = {{NOTE_AS1, NOTE_C1, NOTE_D1, NOTE_DS1, NOTE_F1, NOTE_G1, N
                       {NOTE_AS5, NOTE_C5, NOTE_D5, NOTE_DS5, NOTE_F5, NOTE_G5, NOTE_A5, SILENCE},
                       {NOTE_AS6, NOTE_C6, NOTE_D6, NOTE_DS6, NOTE_F6, NOTE_G6, NOTE_A6, SILENCE}};
 
-int noteDuration[] = {250, 250, 250, 250, 500, 500, 500, 500, 1000, 1000, 1000, 1500};
+int noteDuration[] = {125, 125, 125, 125, 125, 125, 125, 125, 250, 250, 250, 250, 500, 500, 500, 500, 1000, 1000, 1000, 1500};
 
-/**
- * Keeps track of the current octave and note values for the musical scale.
- * The octave value ranges from 0 to 5, and the note value ranges from 0 to 6.
- * These values are used to determine the pitch of the buzzer based on the
- * gyroscope readings.
- */
-int octave = 3, note = 7, duration = 3;
+struct note melodyCurrentNote = {0, 3, 0};
+struct note bassCurrentNote = {0, 0, 0};
 
 // MPU6050 sensor object
 Adafruit_MPU6050 mpu;
-
-/**
- * Calculates the current octave and note based on the gyroscope readings, and returns the corresponding pitch value from the Bb Major scale.
- *
- * The octave value ranges from 0 to 5, and the note value ranges from 0 to 6. These values are updated based on the X and Y gyroscope readings, respectively.
- *
- * @param g The gyroscope event data.
- * @return The pitch value from the Bb Major scale corresponding to the current octave and note.
- */
-unsigned int pitch(sensors_event_t a, sensors_event_t g){
-  float totalSpin = sqrt(g.gyro.x * g.gyro.x + g.gyro.y * g.gyro.y);
-  float totalAcc = sqrt(a.acceleration.x * a.acceleration.x + a.acceleration.y * a.acceleration.y);
-  Serial.println("totalAcc");
-  Serial.println(totalAcc);
-  Serial.println("totalSpin");
-  Serial.println(totalSpin);
-
-  if (totalAcc < 3) octave -= 1;
-  if (totalAcc >= 3) octave += 1;
-
-  if (octave < 0) octave = 5;
-  if (octave > 5) octave = 0;
-  
-  if (totalSpin < 3) note -= random(0, 6);
-  if (totalSpin > 4) note += random(0, 6);
-
-  if (note < 0) note = abs(note);
-  while (note > 6) note -= 3;
-
-  if(totalAcc < 0.5 || totalSpin < 0.5) note = 7;
-
-  return bb_scale[octave][note];
-}
 
 /**
  * @brief Calculates the duration of a note based on accelerometer data.
@@ -81,13 +52,73 @@ unsigned int pitch(sensors_event_t a, sensors_event_t g){
  * @param a The accelerometer event containing acceleration data.
  * @return The duration of the note based on the total acceleration.
  */
-int defineNoteDuration (sensors_event_t acc){
-  float totalAcc = sqrt(acc.acceleration.x * acc.acceleration.x + acc.acceleration.y * acc.acceleration.y);
-  if (totalAcc > 0.5 and totalAcc < 0.75) return noteDuration[random(8, 11)];
-  if (totalAcc > 0.75 and totalAcc < 3) return noteDuration[random(5, 8)];
-  return noteDuration[random(0, 5)];
+int defineNoteDuration (float totalAcc){
+  if (totalAcc > 0.5 and totalAcc < 0.75) return noteDuration[random(18, 19)];
+  if (totalAcc > 0.75 and totalAcc < 3) return noteDuration[random(10, 18)];
+  return noteDuration[random(0, 10)];
 }
 
+/**
+ * Calculates the current octave and note based on the gyroscope readings, and returns the corresponding pitch value from the Bb Major scale.
+ *
+ * The octave value ranges from 0 to 5, and the note value ranges from 0 to 6. These values are updated based on the X and Y gyroscope readings, respectively.
+ *
+ * @param g The gyroscope event data.
+ * @return The pitch value from the Bb Major scale corresponding to the current octave and note.
+ */
+void defineMelodyNote(float totalAcc, float totalSpin){
+  int octave = melodyCurrentNote.octave;
+  int pitch = melodyCurrentNote.pitch;
+
+  if (totalAcc < 3) octave -= 1;
+  if (totalAcc >= 3) octave += 1;
+
+  if (octave < 0) octave = 5;
+  if (octave > 5) octave = 2;
+  
+  if (totalSpin < 3) pitch -= random(0, 6);
+  if (totalSpin > 4) pitch += random(0, 6);
+
+  if (pitch < 0) pitch = abs(pitch);
+  while (pitch > 6) pitch -= 3;
+
+  if(totalAcc < 0.5 || totalSpin < 0.5) pitch = 7;
+
+  melodyCurrentNote.pitch = pitch;
+  melodyCurrentNote.octave = octave;
+  melodyCurrentNote.duration = defineNoteDuration(totalAcc);
+}
+
+void defineBassNote(float totalAcc, float totalSpin){
+  int harmonics[8][3] = {{2, 4, 6}, {3, 5, 0}, {4, 6, 1}, 
+                        {5, 0, 2}, {6, 1, 3}, {0, 2, 4}, 
+                        {1, 3, 5}, {7, 7, 7}};
+  int octave = bassCurrentNote.octave;
+  int pitch = bassCurrentNote.pitch;
+
+  if (totalSpin < 3) octave -= 1;
+  if (totalSpin >= 3) octave += 1;
+
+  if (octave < 0) octave = 2;
+  if (octave > 5) octave = 0;
+  
+  bassCurrentNote.pitch = harmonics[melodyCurrentNote.pitch][random(0, 2)];
+  bassCurrentNote.octave = octave;
+  bassCurrentNote.duration = defineNoteDuration(totalAcc);
+}
+
+void playNote(sensors_event_t a, sensors_event_t g){
+  float totalAcc = sqrt(a.acceleration.x * a.acceleration.x + a.acceleration.y * a.acceleration.y);
+  float totalSpin = sqrt(g.gyro.x * g.gyro.x + g.gyro.y * g.gyro.y);
+  defineMelodyNote(totalAcc, totalSpin);
+  defineBassNote(totalAcc, totalSpin);
+  Serial.println(melodyCurrentNote.duration);
+  Serial.println(melodyCurrentNote.octave);
+  Serial.println(melodyCurrentNote.pitch);
+
+  tone(BUZZZER_PIN_1, bb_scale[melodyCurrentNote.octave][melodyCurrentNote.pitch]);
+  tone(BUZZZER_PIN_2, bb_scale[bassCurrentNote.octave][bassCurrentNote.pitch]);
+}
 
 /**
  * Configures the MPU6050 sensor with the following settings:
@@ -153,10 +184,11 @@ void loop() {
   mpu.getEvent(&a, &g, &temp);
 
   /*sets a pitch to the buzzer according to the rotation on the gyroscope*/
-  tone(BUZZZER_PIN_1, pitch(a, g));
+  playNote(a, g);
 
-  //printMPUData(a, g, temp);
+  printMPUData(a, g, temp);
   
-  delay(defineNoteDuration(a));
+  delay(melodyCurrentNote.duration);
   noTone(BUZZZER_PIN_1);
+  noTone(BUZZZER_PIN_2);
 }
